63905f88e6f3c196abd86243d6bf4731bea7acd9
Connor Sullivan <cksullvn@gmail.com>
2020-05-21 21:36:25 -0500

    initial setup complete. Pending correction of error upon launch

---
+++ CandyApi/Controllers/CandyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CandyApi.DataAccess;
+using CandyApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CandyApi.Controllers
+{
+    [Route("api/candies")]
+    [ApiController]
+    public class CandyController : ControllerBase
+    {
+        CandyRepository _repository;
+
+        public CandyController(CandyRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //api/candies
+        [HttpGet]
+        public IActionResult GetAllCandy()
+        {
+            var allCandies = _repository.GetAll();
+
+            return Ok(allCandies);
+        }
+    }
+}
---
+++ CandyApi/Models/Candy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyApi.Models
+{
+    public class Candy
+    {
+        public int CandyId { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+        public int FlavorId { get; set; }
+        public DateTime DateCollected { get; set; }
+        public bool Ate { get; set; }
+        public int StashId { get; set; }
+    }
+
+    public class Stash
+    {
+        public int StashId { get; set; }
+        public int CandyId { get; set; }
+        public int UserId { get; set; }
+    }
+}
